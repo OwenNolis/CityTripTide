16db47b9e3c157dee737adf053299897afe4963c
OwenNolis <83425846+OwenNolis@users.noreply.github.com>
2025-07-10 22:58:06 +0200

    Add title topbar for favorite list page

--- app/src/main/java/devkonig/citytriptride/FavoriteListScreen.kt
+++ app/src/main/java/devkonig/citytriptride/FavoriteListScreen.kt
@@ -5,10 +5,27 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun FavoriteListScreen(navController: NavController) {
     Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Box(
+                        modifier = Modifier.fillMaxWidth(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text("CityTripTide")
+                    }
+                }
+            )
+        },
         bottomBar = { BottomNavBar(navController, currentRoute = "favorites") }
     ) { innerPadding ->
         Text(
