a59ab8762d3647d2e38fd7bbff1947515959da90
OwenNolis <83425846+OwenNolis@users.noreply.github.com>
2025-07-17 22:23:28 +0200

    Add commentary to code in LocationUtils

--- app/src/main/java/devkonig/citytriptride/LocationUtils.kt
+++ app/src/main/java/devkonig/citytriptride/LocationUtils.kt
@@ -9,6 +9,14 @@ import kotlinx.coroutines.withContext
 import java.util.Locale
 
 object LocationUtils {
+    /**
+     * Retrieves the name of a location based on latitude and longitude.
+     *
+     * @param context The context to use for geocoding.
+     * @param latitude The latitude of the location.
+     * @param longitude The longitude of the location.
+     * @return The name of the location, or null if it could not be determined.
+     */
     suspend fun getLocationName(context: Context, latitude: Double, longitude: Double): String? {
         return withContext(Dispatchers.IO) {
             try {
