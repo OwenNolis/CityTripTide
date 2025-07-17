b4a65c02c2df13ffe49f17071ca826e93884a733
OwenNolis <83425846+OwenNolis@users.noreply.github.com>
2025-07-17 21:30:24 +0200

    Add plugins and dependencies explanation in commentary 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    // Applies the Kotlin Compose plugin for Jetpack Compose support in Kotlin
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.0"
 
+    // Makes the Android application plugin available for subprojects, but does not apply it to this project
     alias(libs.plugins.android.application) apply false
+    // Makes the Kotlin Android plugin available for subprojects, but does not apply it to this project
     alias(libs.plugins.kotlin.android) apply false
 
     // Add the dependency for the Google services Gradle plugin
