1c100eac0b1f6b459b1b9856d5cf94bb127a6241
OwenNolis <83425846+OwenNolis@users.noreply.github.com>
2025-07-18 22:51:00 +0200

    Fix border css for sights in SightsGrid

--- app/src/main/java/devkonig/citytriptride/SightsGrid.kt
+++ app/src/main/java/devkonig/citytriptride/SightsGrid.kt
@@ -2,6 +2,7 @@ package devkonig.citytriptride
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -23,6 +24,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 
@@ -45,7 +47,12 @@ fun SightsGrid(
             Card(
                 modifier = Modifier
                     .aspectRatio(1f)
-                    .clickable { onSightClick(sight) },
+                    .clickable { onSightClick(sight) }
+                    .border(
+                        width = 2.dp,
+                        color = Color(0xFF444444).copy(alpha = 0.4f), // subtle dark gray with lower opacity
+                        shape = MaterialTheme.shapes.medium
+                    ),
                 elevation = 4.dp
             ) {
                 Column(
